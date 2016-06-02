03eb36a98e129a7899b11e8139ba85781bb2ee6f
Ioannis Canellos <iocanel@gmail.com>
2016-06-02 12:34:59 +0300

    Remove invalid import from HttpClientUtils.java

--- client/src/main/java/io/fabric8/docker/client/utils/HttpClientUtils.java
+++ client/src/main/java/io/fabric8/docker/client/utils/HttpClientUtils.java
@@ -47,7 +47,6 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static io.fabric8.docker.client.utils.Utils.isNotNullOrEmpty;
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 public class HttpClientUtils {
 
